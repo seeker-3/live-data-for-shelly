f1c49e0f73cc5270368cd03f5c4fc76b94e2414b
Abram Bradley <abramabradley@gmail.com>
2022-11-05 20:33:32 -0400

    first

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+const PAST_TO_CSV = ''
+
+const div = document.createElement('div')
+
+setInterval(async () => {
+  // const response = await fetch(PAST_TO_CSV)
+  // const data = await response.text()
+  // console.log(d3)
+  // div.innerText = data
+}, 1_000)
+
+export {}
